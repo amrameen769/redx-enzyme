42ba29080b73be4bf64c3d33597f1c408fb3699e
Al Ameen <amrameen769@gmail.com>
2024-02-24 15:25:45 +0530

    header component created

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="main">Test</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="main">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
