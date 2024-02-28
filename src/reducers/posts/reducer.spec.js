a44bd4f961294873b08eaf47945bfbcf4f4e3126
Al Ameen <amrameen769@gmail.com>
2024-02-28 18:57:37 +0530

    added tests for postreducer

---
+++ src/reducers/posts/reducer.spec.js
@@ -0,0 +1,41 @@
+import { types } from "../../actions/types";
+import postsReducer from "./reducer";
+
+describe("postsReducer", () => {
+  it("should return default state", () => {
+    const newState = postsReducer(undefined, {});
+
+    expect(newState).toEqual([]);
+  });
+
+  it("should return new state if receiving action type", () => {
+    const posts = [
+      {
+        userId: 1,
+        id: 1,
+        title:
+          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "qui est esse",
+        body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+      },
+    ];
+
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts,
+    });
+
+    expect(newState).toEqual(posts);
+  });
+});
